cc93dd7307c208601eb9c5fcacb14a9b3eec6f4f
Samir Zenag <samzenag@gmail.com>
2018-08-17 18:13:54 +0200

    should work in prod

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,9 @@
 module CocktailsHelper
+  def cocktail_image_path(cocktail)
+    if cocktail.photo.blank?
+      image_path("z.jpg")
+    else
+      cl_image_path(cocktail.photo, height: 300, width: 400, crop: :fill)
+    end
+  end
 end
