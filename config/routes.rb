82ebf167328f8f3accd72a5671fbde57b16f8142
Samir Zenag <samzenag@gmail.com>
2018-08-17 16:42:30 +0200

    home & about pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   get 'pages/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+  resources :pages, only: [:about] do
+    collection do
+      get 'about', to: 'pages#about', as: :about
+    end
+  end
 end
